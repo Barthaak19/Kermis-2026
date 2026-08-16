a18d03d263df95bd6b990a6d06ca3ff35daf1564
Barthaak19 <gelefietstas@gmail.com>
2026-08-16 02:53:59 +0200

    Fix supabaseAnonKey syntax in friends-config.js

--- friends-config.js
+++ friends-config.js
@@ -1,5 +1,5 @@
 // Alleen publishable/anon key gebruiken. NOOIT de service_role key in GitHub zetten.
 window.KERMIS_FRIENDS_CONFIG={
   supabaseUrl:'https://nieurxneocdntemdihdb.supabase.co',
-  supabaseAnonKey:'sb_publishable_vvwIMrGxJpT3RTocVlBYSg_OGakNw7n'
+sb_publishable_vvwIMrGxJpT3RTocVlBYSg_OGakNw7r
 };
