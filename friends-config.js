45a5100a4fabcb2ad8fb3e298f8e9ecdc1e49bae
Barthaak19 <gelefietstas@gmail.com>
2026-08-16 03:00:01 +0200

    Update KERMIS_FRIENDS_CONFIG with anon key

--- friends-config.js
+++ friends-config.js
@@ -1,5 +1,6 @@
-// Alleen publishable/anon key gebruiken. NOOIT de service_role key in GitHub zetten.
-window.KERMIS_FRIENDS_CONFIG={
-  supabaseUrl:'https://nieurxneocdntemdihdb.supabase.co',
-sb_publishablesb_publishable_vvwIMrGxJpT3RTocVlBYSg_OGakNw7r
+// Alleen publishable/anon key gebruiken. NOOIT de service_role/secret key in GitHub zetten.
+
+window.KERMIS_FRIENDS_CONFIG = {
+  supabaseUrl: 'https://nieurxneocdntemdihdb.supabase.co',
+  supabaseAnonKey: sb_publishable_vvwIMrGxJpT3RTocVlBYSg_OGakNw7r
 };
