ebd412fbb91d4db586ef1200a4c8c19fd79204ff
Barthaak19 <gelefietstas@gmail.com>
2026-08-16 03:01:32 +0200

    Fix formatting of supabaseAnonKey in friends-config.js

--- friends-config.js
+++ friends-config.js
@@ -2,5 +2,5 @@
 
 window.KERMIS_FRIENDS_CONFIG = {
   supabaseUrl: 'https://nieurxneocdntemdihdb.supabase.co',
-  supabaseAnonKey: sb_publishable_vvwIMrGxJpT3RTocVlBYSg_OGakNw7r
+  supabaseAnonKey: 'sb_publishable_vvwIMrGxJpT3RTocVlBYSg_OGakNw7r'
 };
