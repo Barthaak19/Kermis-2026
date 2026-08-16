01f4d550423a17c55882f449639568943ea01b67
Barthaak19 <gelefietstas@gmail.com>
2026-08-16 02:56:54 +0200

    Fix typo in friends-config.js

--- friends-config.js
+++ friends-config.js
@@ -1,5 +1,5 @@
 // Alleen publishable/anon key gebruiken. NOOIT de service_role key in GitHub zetten.
 window.KERMIS_FRIENDS_CONFIG={
   supabaseUrl:'https://nieurxneocdntemdihdb.supabase.co',
-sb_publishable_vvwIMrGxJpT3RTocVlBYSg_OGakNw7r
+sb_publishablesb_publishable_vvwIMrGxJpT3RTocVlBYSg_OGakNw7r
 };
