ab23c17ef781cabb5239b4aaad832afe8d58cd3e
Barthaak19 <gelefietstas@gmail.com>
2026-08-16 02:31:41 +0200

    Add files via upload

---
+++ friends-config.js
@@ -0,0 +1,5 @@
+// Alleen publishable/anon key gebruiken. NOOIT de service_role key in GitHub zetten.
+window.KERMIS_FRIENDS_CONFIG={
+  supabaseUrl:'https://JOUW-PROJECT.supabase.co',
+  supabaseAnonKey:'JOUW_PUBLISHABLE_OF_ANON_KEY'
+};
